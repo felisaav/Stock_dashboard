bb19797f18221e7e7117016a3ada33faaf91503f
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-11-05 22:37:20 -0300

    Update Stock_app.py

--- Stock_app.py
+++ Stock_app.py
@@ -112,10 +112,12 @@ with tab1:
             rsi_values = calculate_rsi(df['Close'])
             df['RSI'] = rsi_values
             df['Signal'] = generate_signals(rsi_values)
-            df['Symbol'] = np.where(df['Signal'] =='BUY', "triangle-up")
-            df['Symbol'] = np.where(df['Signal'] =='SELL', "triangle-down") # triangle up for + day, triangle down for - day
-            df['Color'] = np.where(df['Symbol']=="triangle-up", "green")
-            df['Color'] = np.where(df['Symbol']=="triangle-down", "red") # defining green positive change and red for negative daily change
+            df['Symbol'] = np.where(df['Signal'] == 'BUY', "triangle-up", np.where(df['Signal'] == 'SELL', "triangle-down", None))
+            #df['Symbol'] = np.where(df['Signal'] =='BUY', "triangle-up")
+            #df['Symbol'] = np.where(df['Signal'] =='SELL', "triangle-down") # triangle up for + day, triangle down for - day
+            df['Color'] = np.where(df['Signal'] == 'BUY', "green", np.where(df['Signal'] == 'SELL', "red", None))
+            #df['Color'] = np.where(df['Symbol']=="triangle-up", "green")
+            #df['Color'] = np.where(df['Symbol']=="triangle-down", "red") # defining green positive change and red for negative daily change
     
             #-----
 
